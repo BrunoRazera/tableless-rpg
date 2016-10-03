58b92199a5609950d7b39630d1db4c4403aa0189
Bruno Razera <brunocrazera@gmail.com>
2016-10-03 10:48:37 -0300

    character_info interface

---
+++ TableLess-RPG/app/src/main/java/com/example/bruno/tableless_rpg/activity/CharacterActivity.java
@@ -0,0 +1,15 @@
+package com.example.bruno.tableless_rpg.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.bruno.tableless_rpg.R;
+
+public class CharacterActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+    }
+}
--- TableLess-RPG/app/src/main/java/com/example/bruno/tableless_rpg/activity/MainActivity.java
+++ TableLess-RPG/app/src/main/java/com/example/bruno/tableless_rpg/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.bruno.tableless_rpg;
+package com.example.bruno.tableless_rpg.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.bruno.tableless_rpg.R;
 
 public class MainActivity extends AppCompatActivity {
 
