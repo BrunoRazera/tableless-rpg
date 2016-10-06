0cc90514c2097f71afc4de85d29a263e7ecdceda
Bruno Razera <brunocrazera@gmail.com>
2016-10-06 12:11:51 -0300

    sobe tela de character_info

--- TableLess-RPG/app/src/main/java/com/example/bruno/tableless_rpg/activity/MainActivity.java
+++ TableLess-RPG/app/src/main/java/com/example/bruno/tableless_rpg/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bruno.tableless_rpg.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.bruno.tableless_rpg.R;
 
@@ -11,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View.OnClickListener listnr = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("character_info");
+                startActivity(i);
+            }
+        };
+
+        Button btn = (Button) findViewById(R.id.charInfo);
+        assert btn != null;
+        btn.setOnClickListener(listnr);
     }
 }
