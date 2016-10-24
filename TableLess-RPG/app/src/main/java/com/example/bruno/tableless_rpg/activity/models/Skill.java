776027eb2e939dd556aab003884bc4d552a630bc
Bruno Razera <brunocrazera@gmail.com>
2016-10-24 10:48:40 -0200

    implementa classe skill

---
+++ TableLess-RPG/app/src/main/java/com/example/bruno/tableless_rpg/activity/models/Skill.java
@@ -0,0 +1,13 @@
+package com.example.bruno.tableless_rpg.activity.models;
+
+/**
+ * Created by bruno on 24/10/16.
+ */
+public class Skill {
+    private String id;
+    private String skillName;
+    private String keyAbility;
+    private String description;
+    private Efect efect;
+    private String requirements;
+}
